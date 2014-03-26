d905d9fa7e59fb1533bf3e1d7401509bfafaf00e
linhtranhna <linhtran.hna@gmail.com>
2014-03-26 14:33:51 +0700

    ok

--- application/controllers/detail.php
+++ application/controllers/detail.php
@@ -5,13 +5,24 @@ class detail extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('video_model');
 	}
 
-	public function index()
+	public function index($slug)
 	{
-		$data['title'] = 'Funny & Meaning :: Hài hước & Ý nghĩa';
-		$data['new_videos'] = $this->video_model->get_all(2);
-        $this->config->set_item('frontend_layout','full_page');
-		$this->load->view('home/index',$data);
+		$data['detail'] = $this->video_model->getDetail($slug,1);
+		if($data['detail']==0)
+		{
+			show_404('page' ,['log_error']);
+			$this->config->set_item('frontend_layout','full_page');
+			$this->load->view('detail/index',$data);
+		}
+		else
+		{
+			$this->config->set_item('frontend_layout','full_page');
+			$this->load->view('detail/index',$data);
+		}
+		// $data['new_videos'] = $this->video_model->get_all(2);
+
 	}
 }
--- application/models/video_model.php
+++ application/models/video_model.php
@@ -5,5 +5,19 @@ class video_model extends MY_Model
     protected $_table = 'tbl_videos';
     protected $primary_key = 'video_id';
     // protected $_entity_name = 'admin_entity';
-    
+    public function getDetail($slug,$status)
+    {
+    	if($slug!=null)
+    	{
+	    	$array = array('post_slug' => $slug, 'post_status' => 1);
+	    	$this->db->select('*');
+	    	$this->db->from($this->_table);
+	    	$this->db->where($array);
+	    	return $this->db->get();
+    	}
+    	else
+    	{
+    		return 0;
+    	}
+    }
 }
