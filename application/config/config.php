a5b73751812b37814401bc880384136c66719593
vietlt <viet.lt@icloud.com>
2014-04-22 15:43:09 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ function __autoload($class) {
 | path to your installation...
 |
 */
-$config['base_url']	= 'http://localhost/funmea/';
+$config['base_url']	= 'http://funmea.com/';
 
 /*
 |--------------------------------------------------------------------------
